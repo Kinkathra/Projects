4cd17c2b7582642a1111eb3e1e315362c0d2b1dc
Kinkathra <69177879+Kinkathra@users.noreply.github.com>
2020-10-11 12:15:22 -0500

    Create emp.js

---
+++ emp.js
@@ -0,0 +1,465 @@
+function myFunc(){
+var userInput = [input00, input0, input3].value
+
+for(i=0; i<2; i++){
+	console.log(userInput)
+}}
+
+
+
+// $("input").addClass("inpOnE");
+
+// $(".noteInput1").keypress(function(e){
+// 	if(e.which === 13){
+// 	var x = document.querySelector(".noteInput1").value;
+// 	var i;
+// 	var y = document.createElement("h3");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector(".notes1");
+// 	a.appendChild(y);
+// 	document.querySelector(".noteInput1").value="";//Clear Text		
+// 		}
+// })
+// $(".noteInput2").keypress(function(e){
+// 	if(e.which === 13){
+// 	var x = document.querySelector(".noteInput1").value;
+// 	var i;
+// 	var y = document.createElement("h3");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector(".notes1");
+// 	a.appendChild(y);
+// 	document.querySelector(".noteInput1").value="";//Clear Text			
+// 		}
+// })$(".noteInput3").keypress(function(e){
+// 	if(e.which === 13){
+// 	var x = document.querySelector(".noteInput1").value;
+// 	var i;
+// 	var y = document.createElement("h3");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector(".notes1");
+// 	a.appendChild(y);
+// 	document.querySelector(".noteInput1").value="";//Clear Text			
+// 		}
+// })$(".noteInput4").keypress(function(e){
+// 	if(e.which === 13){
+// 	var x = document.querySelector(".noteInput1").value;
+// 	var i;
+// 	var y = document.createElement("h3");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector(".notes1");
+// 	a.appendChild(y);
+// 	document.querySelector(".noteInput1").value="";//Clear Text			
+// 		}
+// })$(".noteInput5").keypress(function(e){
+// 	if(e.which === 13){
+// 	var x = document.querySelector(".noteInput1").value;
+// 	var i;
+// 	var y = document.createElement("h3");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector(".notes1");
+// 	a.appendChild(y);
+// 	document.querySelector(".noteInput1").value="";//Clear Text			
+// 		}
+// })$(".noteInput6").keypress(function(e){
+// 	if(e.which === 13){
+// 	var x = document.querySelector(".noteInput1").value;
+// 	var i;
+// 	var y = document.createElement("h3");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector(".notes1");
+// 	a.appendChild(y);
+// 	document.querySelector(".noteInput1").value="";//Clear Text			
+// 		}
+// })$(".noteInput7").keypress(function(e){
+// 	if(e.which === 13){
+// 	var x = document.querySelector(".noteInput1").value;
+// 	var i;
+// 	var y = document.createElement("h3");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector(".notes1");
+// 	a.appendChild(y);
+// 	document.querySelector(".noteInput1").value="";//Clear Text			
+// 		}
+// })$(".noteInput8").keypress(function(e){
+// 	if(e.which === 13){
+// 	var x = document.querySelector(".noteInput1").value;
+// 	var i;
+// 	var y = document.createElement("h3");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector(".notes1");
+// 	a.appendChild(y);
+// 	document.querySelector(".noteInput1").value="";//Clear Text			
+// 		}
+// })
+
+// function addNote1(){
+// 	var addNote1 = document.querySelector("#addNote1");
+// 	var noteInput = document.querySelector(".noteInput1").value;
+// 	var notes = document.querySelector(".notes1");
+// 	var x = document.createElement("h3");
+// 	var y = document.createTextNode(noteInput);
+// 	x.appendChild(y);
+// 	var note = document.querySelector(".notes1")
+// 	note.appendChild(x)
+	
+// }
+
+// function addNote2(){
+// 	var addNote1 = document.querySelector("#addNote2");
+// 	var noteInput = document.querySelector(".noteInput2").value;
+// 	var notes = document.querySelector(".notes2");
+// 	var x = document.createElement("h3");
+// 	var y = document.createTextNode(noteInput);
+// 	x.appendChild(y);
+// 	var note = document.querySelector(".notes2")
+// 	note.appendChild(x)
+	
+// }
+
+// function addNote3(){
+// 	var addNote1 = document.querySelector("#addNote3");
+// 	var noteInput = document.querySelector(".noteInput3").value;
+// 	var notes = document.querySelector(".notes3");
+// 	var x = document.createElement("h3");
+// 	var y = document.createTextNode(noteInput);
+// 	x.appendChild(y);
+// 	var note = document.querySelector(".notes3")
+// 	note.appendChild(x)
+	
+// }
+
+// function addNote4(){
+// 	var addNote1 = document.querySelector("#addNote4");
+// 	var noteInput = document.querySelector(".noteInput4").value;
+// 	var notes = document.querySelector(".notes4");
+// 	var x = document.createElement("h3");
+// 	var y = document.createTextNode(noteInput);
+// 	x.appendChild(y);
+// 	var note = document.querySelector(".notes4")
+// 	note.appendChild(x)
+	
+// }
+
+// function addNote5(){
+// 	var addNote1 = document.querySelector("#addNote5");
+// 	var noteInput = document.querySelector(".noteInput5").value;
+// 	var notes = document.querySelector(".notes5");
+// 	var x = document.createElement("h3");
+// 	var y = document.createTextNode(noteInput);
+// 	x.appendChild(y);
+// 	var note = document.querySelector(".notes5")
+// 	note.appendChild(x)
+	
+// }
+
+// function addNote6(){
+// 	var addNote1 = document.querySelector("#addNote6");
+// 	var noteInput = document.querySelector(".noteInput6").value;
+// 	var notes = document.querySelector(".notes6");
+// 	var x = document.createElement("h3");
+// 	var y = document.createTextNode(noteInput);
+// 	x.appendChild(y);
+// 	var note = document.querySelector(".notes6")
+// 	note.appendChild(x)
+	
+// }
+
+// function addNote7(){
+// 	var addNote1 = document.querySelector("#addNote7");
+// 	var noteInput = document.querySelector(".noteInput7").value;
+// 	var notes = document.querySelector(".notes7");
+// 	var x = document.createElement("h3");
+// 	var y = document.createTextNode(noteInput);
+// 	x.appendChild(y);
+// 	var note = document.querySelector(".notes7")
+// 	note.appendChild(x)
+	
+// }
+
+// function addNote8(){
+// 	var addNote1 = document.querySelector("#addNote8");
+// 	var noteInput = document.querySelector(".noteInput8").value;
+// 	var notes = document.querySelector(".notes8");
+// 	var x = document.createElement("h3");
+// 	var y = document.createTextNode(noteInput);
+// 	x.appendChild(y);
+// 	var note = document.querySelector(".notes8")
+// 	note.appendChild(x)
+	
+// }
+
+// // document.getElementById(this).addEventListener("keypress", function(e){
+// // 	if(e.key === "Enter"){
+// // 		alert("Ey	yyy")
+// // 	}
+// // })
+
+
+// // var zz, o,t,th,fr,f,s;
+// // for(var i = 0; i <= 7; i ++){
+// // var str = "input" + [i];
+// // console.log(str)
+// // }
+
+
+
+
+
+// // $("input").addClass("inpOnE");
+
+// // [...document.querySelectorAll('.inpOnE')].forEach(function(item) {
+// //   item.addEventListener('keypress', function(e) {
+// // 	  if(e.key === "Enter"){
+// // 	var x;
+// // 	x = document.getElementById("input00").value;
+// // 	var i;
+// // 	var y = document.createElement("h2");
+// // 	var z = document.createTextNode(x);
+// // 	y.appendChild(z);
+// // 	var a = document.querySelector("#names");
+// // 	a.appendChild(y);
+// // 		  }    
+// // 	  ;
+// //   });
+// //    });//Makes each input for Adding Each Employee "Enterable" on Kepress
+
+// // [...document.querySelectorAll('.inpOnE')].forEach(function(item) {
+// //   item.addEventListener('keypress', function(e) {
+// // 	  if(e.key === "Enter"){
+// // 	var x;
+// // 	x = document.getElementById("input0").value;
+// // 	var i;
+// // 	var y = document.createElement("h2");
+// // 	var z = document.createTextNode(x);
+// // 	y.appendChild(z);
+// // 	var a = document.querySelector("#names0");
+// // 	a.appendChild(y);
+		  
+// // 		  }
+// //   });
+// //    });//Makes each input for Adding Each Employee "Enterable" on Kepress
+
+
+
+// // forEach("keypress", function(e){if(e.key === "Enter"){
+// // 	for( i=0;i<=0;i++){
+// // 	var x;
+// // 	x = document.getElementById("input00").value;
+// // 	var i;
+// // 	var y = document.createElement("h2");
+// // 	var z = document.createTextNode(x);
+// // 	y.appendChild(z);
+// // 	var a = document.querySelector("#names");
+// // 	a.appendChild(y)
+// // 	}
+// // }
+// // })
+// $("#input00").keypress(function(e){
+// 		if(e.which === 13){
+// 				var x;
+// 	x = document.getElementById("input00").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names");
+// 	a.appendChild(y);
+// 			document.getElementById("input00").value="";//Clear Text
+// 		}
+// })
+
+// $("#input0").keypress(function(e){
+// 		if(e.which === 13){
+// 				var x;
+// 	x = document.getElementById("input0").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names0");
+// 	a.appendChild(y);
+// 			document.getElementById("input0").value="";//Clear Text			
+// 		}
+// })
+// $("#input1").keypress(function(e){
+// 		if(e.which === 13){
+// 				var x;
+// 	x = document.getElementById("input1").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names1");
+// 	a.appendChild(y);
+// 			document.getElementById("input1").value="";//Clear Text			
+// 		}
+// })
+
+// $("#input2").keypress(function(e){
+// 		if(e.which === 13){
+// 				var x;
+// 	x = document.getElementById("input2").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names2");
+// 	a.appendChild(y);
+// 			document.getElementById("input2").value="";//Clear Text			
+// 		}
+// })
+
+// $("#input3").keypress(function(e){
+// 		if(e.which === 13){
+// 				var x;
+// 	x = document.getElementById("input3").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names4");
+// 	a.appendChild(y);
+// 			document.getElementById("input03").value="";//Clear Text			
+// 		}
+// })
+
+// $("#input5").keypress(function(e){
+// 		if(e.which === 13){
+// 				var x;
+// 	x = document.getElementById("input5").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names5");
+// 	a.appendChild(y);
+// 			document.getElementById("input05").value="";//Clear Text			
+// 		}
+// })
+
+// $("#input6").keypress(function(e){
+// 		if(e.which === 13){
+// 				var x;
+// 	x = document.getElementById("input6").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names6");
+// 	a.appendChild(y);
+// 			document.getElementById("input6").value="";//Clear Text			
+// 		}
+// })//To Allow Enter on Input
+
+// function myFunc(){
+// 	for( i=0;i<=0;i++){
+// 	var x;
+// 	x = document.getElementById("input00").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names");
+// 	a.appendChild(y)
+// 	}
+// }
+
+// function myFunc0(){
+// 	for(i=0; i<=0; i++){
+// 	var x;
+// 	x = document.getElementById("input0").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names0");
+// 	a.appendChild(y)
+// 	}
+// }
+
+// function myFunc1(){
+// 	for(i=0;i<=0;i++){
+// 	var x;
+// 	x = document.getElementById("input1").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names1");
+// 	a.appendChild(y)
+// 	}
+// }
+
+// function myFunc2(){
+// 	for(i=0;i<=0;i++){
+// 	var x;
+// 	x = document.getElementById("input2").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names2");
+// 	a.appendChild(y)
+// 	}
+// }
+
+// function myFunc3(){
+// 	for(i=0;i<=0;i++){
+// 	var x;
+// 	x = document.getElementById("input3").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names3");
+// 	a.appendChild(y)
+// 	}
+// }
+
+// function myFunc4(){
+// 	for(i=0;i<=0;i++){
+// 	var x;
+// 	x = document.getElementById("input4").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names4");
+// 	a.appendChild(y)
+// 	}
+// }
+
+// function myFunc5(){
+// 	for(i=0;i<=0;i++){
+// 	var x;
+// 	x = document.getElementById("input5").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names5");
+// 	a.appendChild(y)
+// 	}
+// }
+
+// function myFunc6(){
+// 	for(i=0;i<=0;i++){
+// 	var x;
+// 	x = document.getElementById("input6").value;
+// 	var i;
+// 	var y = document.createElement("h2");
+// 	var z = document.createTextNode(x);
+// 	y.appendChild(z);
+// 	var a = document.querySelector("#names6");
+// 	a.appendChild(y)
+// 	}
+// }
+
